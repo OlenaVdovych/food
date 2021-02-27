05966b3d4ec2d3ffca5afd25362581f5b9b3a8ff
Olena Vdovych <olenavdovych17@gmail.com>
2021-02-27 22:20:22 +0200

    Added class for cards - MenuCard

--- js/script.js
+++ js/script.js
@@ -130,7 +130,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    const modalTimer = setTimeout(openModal, 5000);
+    // const modalTimer = setTimeout(openModal, 5000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -140,4 +140,67 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    //Classes for cards
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = +this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Вартість:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фітнес"',
+        'Меню "Фітнес" - це новий підхід до приготування страв: більше свіжих овочів і фруктів. Продукт активних і здорових людей. Це абсолютно новий продукт з оптимальною ціною і високою якістю!',
+        9,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Преміум”',
+        'В меню "Преміум" ми використовуємо не тільки гарний дизайн упаковки, але і якісне виконання страв. Червона риба, морепродукти, фрукти - ресторанне меню без походу в ресторан!',
+        18,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Пісне"',
+        'Меню "Пісне" - це ретельний підбір інгредієнтів: повна відсутність продуктів тваринного походження, молоко з мигдалю, вівса, кокоса або гречки, правильна кількість білків за рахунок тофу і імпортних вегетаріанських стейків. ',
+        12,
+        '.menu .container'
+    ).render();
 });
